57692b5dfcc178a764e39f55d35e05bbdf4ffc61
dhatrishdixit <dhatrish29@gmail.com>
2025-02-09 00:50:58 +0530

    working on more and learning about formik

--- project3PasswordGenerator/App.tsx
+++ project3PasswordGenerator/App.tsx
@@ -1,4 +1,4 @@
-import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React,{useState} from 'react';
 import { Formik } from 'formik';
 import * as yup from "yup";
@@ -68,8 +68,11 @@ export default function App() {
 
   return (
     <ScrollView keyboardShouldPersistTaps="handled">
-    <SafeAreaView>
-    <Formik
+    <SafeAreaView style={styles.appContainer}>
+      <View style={styles.formContainer}>
+        <Text style={styles.title}>Password Generator</Text>
+      
+      <Formik
      initialValues={{ passwordLength: '' }}
      validationSchema={passwordSchema}
      onSubmit={values => {
@@ -86,17 +89,140 @@ export default function App() {
         handleReset,
         handleSubmit
       }) => (
-       <View>
-         <TextInput
-    
-         />
-         <Button onPress={handleSubmit} title="Submit" />
-       </View>
+       <>
+         <View style={styles.inputWrapper}>
+           <View style={styles.inputRow}>
+            <Text style={styles.heading}>Password Length</Text>
+            {
+              touched?.passwordLength && errors?.passwordLength ? (<Text style={styles.errorText}>   
+                  {errors.passwordLength}
+               </Text>) 
+               :
+                null
+        }
+             <TextInput
+               style={styles.inputStyle}
+               value={values.passwordLength}
+               onChangeText={handleChange('passwordLength')} 
+               placeholder="ex. 8"
+               keyboardType="numeric"           
+             />
+           </View>
+         </View>
+         <View style={styles.inputWrapper}></View>
+         <View style={styles.inputWrapper}></View>
+         <View style={styles.inputWrapper}></View>
+         <View style={styles.inputWrapper}></View>
+
+         <View style={styles.formActions}>
+            <TouchableOpacity><Text>Generate Password</Text></TouchableOpacity>
+            <TouchableOpacity><Text>Reset</Text></TouchableOpacity>
+         </View>
+       </>
      )}
    </Formik>
+      </View>
     </SafeAreaView>
     </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+  formContainer: {
+    margin: 8,
+    padding: 8,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '600',
+    marginBottom: 15,
+    color: 'white',
+  },
+  subTitle: {
+    fontSize: 26,
+    fontWeight: '600',
+    marginBottom: 2,
+  },
+  description: {
+    color: '#758283',
+    marginBottom: 8,
+  },
+  heading: {
+    fontSize: 15,
+    color:"white"
+  },
+  inputWrapper: {
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  inputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    width: '100%'
+  },
+  inputStyle: {
+    padding: 8,
+    width: '40%',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#16213e',
+  },
+  errorText: {
+    fontSize: 12,
+    color: '#ff0d10',
+  },
+  formActions: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  primaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#5DA3FA',
+  },
+  primaryBtnTxt: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+  secondaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#CAD5E2',
+  },
+  secondaryBtnTxt: {
+    textAlign: 'center',
+  },
+  card: {
+    padding: 12,
+    borderRadius: 6,
+    marginHorizontal: 12,
+  },
+  cardElevated: {
+    backgroundColor: '#ffffff',
+    elevation: 1,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  generatedPassword: {
+    fontSize: 22,
+    textAlign: 'center',
+    marginBottom: 12,
+    color:'#000'
+  },
+});
