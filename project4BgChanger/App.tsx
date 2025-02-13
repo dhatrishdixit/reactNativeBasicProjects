5178a3c945dcfd999ce4e7551e6b7e7a55ecd518
dhatrishdixit <dhatrish29@gmail.com>
2025-02-13 18:59:47 +0530

    working on background project almost done

--- project4BgChanger/App.tsx
+++ project4BgChanger/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -6,6 +6,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
@@ -25,12 +26,49 @@ type SectionProps = PropsWithChildren<{
 
 function App(): React.JSX.Element {
   
+  function generateColor(){
+    const hexValue = "0123456789ABCDEF"
+    let color = "#";
+
+    for(let i=0;i<6;i++){
+      color += hexValue.charAt(Math.floor(Math.random()*hexValue.length));
+    }
+
+    return color ;
+  }
+
+  const [colorBackground,setColorBackground] =  useState<string>("#000000");
+
+  const [colorOne,setColorOne] =  useState<string>("#000000");
+  const [colorTwo,setColorTwo] =  useState<string>("#000000");
+  const [colorThree,setColorThree] =  useState<string>("#000000");
+  const [colorFour,setColorFour] =  useState<string>("#000000");
+
+  
+  function randomiseColor(){
+      setColorBackground(generateColor());
+      setColorOne(generateColor());
+      setColorTwo(generateColor());
+      setColorThree(generateColor());
+      setColorFour(generateColor());
+  }
 
   return (
     <SafeAreaView >
-      <StatusBar/>
-      <View style=[styles.rectangle,{backgroundColor : '#000000'}]>
-        
+      <StatusBar backgroundColor={"#000000"}/>
+      <View style={[styles.background,{backgroundColor:colorBackground}]}>
+        <View style={[styles.rectangle,{backgroundColor:colorOne}]}>
+
+        </View>
+        <View style={[styles.circle,{backgroundColor:colorTwo}]}></View>
+        <View style={[styles.square,{backgroundColor:colorThree}]}></View>
+        <View style={[styles.shapeFour,{backgroundColor:colorFour}]}></View>
+
+      <TouchableOpacity onPress={randomiseColor}>
+        <Text>
+          click to change the color           
+        </Text>
+        </TouchableOpacity>
 
       </View>
       
@@ -39,8 +77,27 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
+    background:{
+       flex:1,
+       flexDirection:"column"
+    },
     rectangle:{
-
+      width:40,
+      height: 20 
+    },
+    circle:{
+      width:40,
+      height:40,
+      borderRadius:80
+    },
+    square:{
+      width:40,
+      height:40
+    },
+    shapeFour:{
+      width:40,
+      height: 20,
+      borderRadius:20,
     }
 });
 
