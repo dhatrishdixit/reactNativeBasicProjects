a22df279d31347fc4db016706dcfe1833458e52e
dhatrishdixit <dhatrish29@gmail.com>
2025-02-17 10:00:53 +0530

    done with this now have to move it other project file

--- project7TicTacToe/src/App.tsx
+++ project7TicTacToe/src/App.tsx
@@ -7,6 +7,9 @@ import {
   Text,
   useColorScheme,
   View,
+  FlatList,
+  TextInput,
+  Pressable
 } from 'react-native';
 import { currencyByRupee } from './constants';
 import { CurrencyButton } from './components/currencyButton';
@@ -16,15 +19,83 @@ import Snackbar from 'react-native-snackbar';
 
 function App(): React.JSX.Element {
 
+  const [inputValue,setInputValue] = React.useState('');
+  const [resultValue,setResultValue] = React.useState('');
+  const [targetCurrency,setTargetCurrency] = React.useState('');
+
+  const buttonPressed = (currency : Currency) => {
+     if(!inputValue){
+       return Snackbar.show({
+        text: "Enter a value to convert",
+        backgroundColor: "#EA7773",
+        textColor: "#000000"
+       })
+     }
+
+     const value = parseFloat(inputValue);
+     if(!isNaN(value)){
+      const convertedValue = value * currency.value;
+      setResultValue(`₹ ${value} ==> ${currency.symbol} ${convertedValue.toFixed(2)}`);
+      setTargetCurrency(currency.name);
+      setInputValue('');
+     }else{
+       return Snackbar.show({
+        text: "Enter Numerical Value",
+        backgroundColor: "#F4BE2C",
+        textColor: "#000000"
+       })
+     }
+  } 
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex:1}}>
     
     { 
       // TODO: Move the dependency also after completing this project into the other one
       
      // TODO: Imp   flexWrap: 'wrap' // this is imp to wrap the view in next line
-    }
 
+     // TODO: imp flex: 1 // this is imp to make the root element take the full dimension of the screen
+    }
+    
+    <StatusBar/>
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <View style={styles.rupeesContainer}>
+        
+        <TextInput
+         maxLength={14}
+         value={inputValue}
+         onChangeText={setInputValue}
+         keyboardType='number-pad'
+         placeholder='Enter Amount in Rupees'
+         clearButtonMode='always'
+        />
+         {resultValue && (
+            <Text style={styles.resultTxt} >
+              {resultValue}
+            </Text>
+          )}
+
+        </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <FlatList
+         numColumns={3}
+         data={currencyByRupee}
+         keyExtractor={item => item.name}
+         renderItem={({item})=>{
+           console.log(item)         
+           return <Pressable
+           style={[styles.button,targetCurrency === item.name && styles.selected]}
+           onPress={()=>buttonPressed(item)}
+           >
+            <CurrencyButton name={item.name} flag={item.flag} />
+           </Pressable>
+         }}
+        />
+      </View>
+    </View>
   
     </SafeAreaView>
   );
@@ -54,7 +125,7 @@ const styles = StyleSheet.create({
     fontWeight: '800',
   },
   rupeesContainer: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'center',
   },
   inputAmountField: {
