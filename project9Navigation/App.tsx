81bac3461cb97311db815be038900f1a62316481
dhatrishdixit <dhatrish29@gmail.com>
2025-03-30 15:21:12 +0530

    started working on navigation in react native

---
+++ project9Navigation/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+
+export default App;
