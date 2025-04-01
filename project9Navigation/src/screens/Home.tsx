eb65bc0509e993a95172862d78ae3b5058da43b1
dhatrishdixit <dhatrish29@gmail.com>
2025-04-01 21:44:01 +0530

    started working on auth in react native

--- project9Navigation/src/screens/Home.tsx
+++ project9Navigation/src/screens/Home.tsx
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
         alignItems:'center'
     },
     headingText: {
-      color: 'white',
+      color: 'black',
       fontSize: 20,
       textAlign: "center" as "center",
     },
